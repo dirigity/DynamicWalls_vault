59cc27f9448698c83360a3ab982f0da230714363
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2022-03-30 09:57:29 +0200

    openverse

---
+++ openverse/build/openverse_index_build.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+
+let res = {};
+const openverse_lib = JSON.parse(fs.readFileSync("openverse/build/openverse.json"))
+
+for (let p of openverse_lib) {
+
+    let path = "openverse/media/" + p.id + "/";
+    let file = path + "head.json";
+    let built_tags = [];
+    for (let t of p.tags) {
+        built_tags.push(t.name)
+    }
+    let classification = {
+        "title": p.title,
+        "tags": built_tags,
+        "description": null,
+        "author": p.creator,
+        "authot_link": p.creator_url,
+        "license": p.license + "-" + p.license_version,
+        "license_link": p.license_url
+    }
+
+    let head = {
+        "id": 0,
+        "type": "url",
+        "imgUrl": p.url,
+        "classification": classification
+    }
+    fs.mkdirSync(path, { recursive: true })
+    fs.writeFileSync(file, JSON.stringify(head));
+
+
+
+    res[file] = classification;
+}
+
+fs.writeFileSync("openverse_index.json", JSON.stringify(res));
---
+++ vault/build/build_vault_index.js
@@ -0,0 +1,201 @@
+const fs = require("fs");
+const { writeFile } = require("node:fs/promises");
+
+function readWord(str, i, debug) {
+    // console.log(debug + "word", i)
+
+    if (str[i] != '"') {
+        // console.log("debug: " + str.substring(i, i + 15))
+        throw "cagaste " + i;
+    }
+    i++;
+    let ret = "";
+    while (str[i] != '"') {
+        ret += str[i];
+        i++;
+    }
+    i++;
+
+    return {
+        "word": ret,
+        "i": i
+    }
+}
+
+function readSynonyms(str, i, debug) {
+    // console.log(debug + "synonims", i)
+    if (str[i] != "(") {
+
+        // console.log(debug + "  empty synonyms at: ", i)
+        return {
+            "synonyms": [],
+            "i": i
+        }
+    }
+
+    let ret = [];
+    while (str[i] == "," || str[i] == "(") {
+        i++;
+        let { word, i: i_ } = readWord(str, i, debug + "   ");
+        i = i_;
+        ret.push(word);
+    }
+
+    // console.log(debug + " found synonyms: ", ret)
+    i++;
+
+    return {
+        "synonyms": ret,
+        "i": i
+    }
+
+
+}
+
+function readSubTags(str, i, debug) {
+    // console.log(debug + "subtags", i)
+
+    if (str[i] == "{") {
+        i++;
+    } else {
+        // console.log(debug + "  empty subtags at: ", i)
+        return {
+            "sub_tags": {},
+            "i": i
+        }
+    }
+    let ret = {};
+
+    while (str[i] != "}") {
+        let { name, ret: tag, i: i_ } = readTag(str, i, debug + "   ");
+        i = i_;
+        ret[name] = tag;
+    }
+
+    // console.log(debug + "  >", ret, str[i]);
+
+    i++;
+    return {
+        "sub_tags": ret,
+        "i": i
+    }
+}
+
+function readTag(str, i, debug) {
+    // console.log(debug + "tag", i)
+    let { word: name, i: i_ } = readWord(str, i, debug + "   ");
+    i = i_
+    let { synonyms, i: i__ } = readSynonyms(str, i, debug + "   ");
+    i = i__
+    let { sub_tags, i: i___ } = readSubTags(str, i, debug + "   ");
+    i = i___
+
+    ret = {
+        "synonyms": synonyms,
+        "sub_tags": sub_tags,
+    };
+
+    return {
+        "i": i,
+        "name": name,
+        "ret": ret,
+    }
+
+}
+
+
+function collect_tags(tag_hierarchy) {
+    let tags = [];
+    for (let tag in tag_hierarchy) {
+        tags.push(tag);
+        tags = tags.concat(tag_hierarchy[tag].synonyms)
+        tags = tags.concat(collect_tags(tag_hierarchy[tag].sub_tags))
+    }
+    return tags;
+}
+
+(() => {
+
+
+    fs.readFile("tag_hierarchy.tree", { encoding: 'utf8', flag: 'r' }, (err, data) => {
+
+        let i = 0;
+        let str = "";
+        let inside_a_literal = false;
+        // console.log(data)
+        while (i < data.length) {
+            if (data[i] == '"') {
+                inside_a_literal = !inside_a_literal;
+            }
+
+            if ((data[i] != " " && data[i] != "\n" && data[i] != "\r") || inside_a_literal) {
+                str += data[i];
+            } else {
+                //console.log(">", data[i]);
+            }
+            i++;
+            //console.log("> " + str + " <");
+
+        }
+        //console.log("> " + str + " <");
+
+        let tag_hierarchy = readTag(str, 0, "").ret.sub_tags
+
+        fs.writeFile("tag_hierarchy.json", JSON.stringify(tag_hierarchy), () => { });
+
+        let tags = collect_tags(tag_hierarchy);
+
+        console.log("collected tags: ", tags);
+
+        fs.readdir("vault", async (err, files) => {
+
+            if (err) {
+                throw err;
+            }
+
+            let res = {};
+            let found_ids = {};
+
+            Promise.all(files.map(file => {
+                return new Promise((r) => {
+                    fs.readFile("vault/" + file + "/head.json", { encoding: 'utf8', flag: 'r' }, (err, raw_data) => {
+
+                        const data = JSON.parse(raw_data);
+
+                        //console.log(data)
+                        for (let head_tag of data.classification.tags) {
+                            let found = false;
+
+                            for (let existing_tag of tags) {
+                                if (existing_tag == head_tag) {
+                                    found = true;
+                                }
+                            }
+
+                            if (!found) {
+                                throw "unhiearchyfied tag: " + head_tag;
+                            }
+                        }
+
+                        if (data.id in found_ids) {
+                            throw "id: " + data.id + " is duplicated";
+                        }
+                        found_ids[data.id] = true;
+
+
+                        res["vault/" + file + "/head.json"] = data.classification;
+
+                        r();
+                    })
+                })
+            })).then(() => {
+                // console.log(res)
+                writeFile("index.json", JSON.stringify(res));
+            })
+
+
+        });
+    })
+
+
+})();
