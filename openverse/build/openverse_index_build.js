45e9d684a3ab7f78b776654d502ee6a5814519c8
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2022-03-31 08:53:50 +0200

    added thumbnails

--- openverse/build/openverse_index_build.js
+++ openverse/build/openverse_index_build.js
@@ -4,14 +4,48 @@ const fs = require("fs");
 let res = {};
 const openverse_lib = JSON.parse(fs.readFileSync("openverse/build/openverse.json"))
 
+function collect_tags(tag_hierarchy) {
+    let tags = [];
+    for (let tag in tag_hierarchy) {
+        tags.push(tag);
+        tags = tags.concat(tag_hierarchy[tag].synonyms)
+        tags = tags.concat(collect_tags(tag_hierarchy[tag].sub_tags))
+    }
+    return tags;
+}
+
+let tag_hierarchy = JSON.parse(fs.readFileSync("tag_system/tag_hierarchy.json"));
+
+let tags = collect_tags(tag_hierarchy);
+
+let error = false;
+let unhiearchyfied_tags = [];
 for (let p of openverse_lib) {
 
     let path = "openverse/media/" + p.id + "/";
     let file = path + "head.json";
     let built_tags = [];
     for (let t of p.tags) {
-        built_tags.push(t.name)
+        let found = false;
+
+        for (let existing_tag of tags) {
+            if (existing_tag == t.name) {
+                found = true;
+            }
+        }
+
+        if (!found) {
+            error = true;
+            unhiearchyfied_tags.push(t.name);
+        }else{
+            built_tags.push(t.name)
+
+        }
+        
+
+
     }
+
     let classification = {
         "title": p.title,
         "tags": built_tags,
@@ -25,14 +59,18 @@ for (let p of openverse_lib) {
     let head = {
         "type": "url-img",
         "imgUrl": p.url,
+        "thumbUrl": p.thumbnail,
         "classification": classification
     }
+
     fs.mkdirSync(path, { recursive: true })
     fs.writeFileSync(file, JSON.stringify(head));
 
-
-
     res[file] = classification;
+
 }
 
-fs.writeFileSync("openverse_index.json", JSON.stringify(res));
+if (!error) {
+    fs.writeFileSync("openverse_index.json", JSON.stringify(res));
+}
+console.log(unhiearchyfied_tags);
